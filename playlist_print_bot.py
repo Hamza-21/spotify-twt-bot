b8cfd6e180c3d29f36de207dc78affe8af93c0e2
Hamza Ansari <69684098+Hamza-21@users.noreply.github.com>
2021-09-20 22:29:58 +0530

    Update playlist_print_bot.py

--- playlist_print_bot.py
+++ playlist_print_bot.py
@@ -4,10 +4,10 @@ from tweepy.models import Media
 from time import sleep
 
 def twtTests():
-    consumer_key = 'rKYbtbfD9EvcMFzSYTDkucht7'
-    consumer_secret = 'gEDV1EEPem36fsKnG68QVphCm0sd6lMn2Crap9UNPZnfxI36ju'
-    access_token = '1436718387099357188-Wcgh99lTznRf4ViwIhpTwOam29HdJR'
-    access_token_secret = 'uJvM3m34RGrbTFVSWzBDTIFKUiOj82zLkC0y2u2QR4CJS'
+    consumer_key = '#'
+    consumer_secret = '#'
+    access_token = '#-#'
+    access_token_secret = '#'
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
@@ -17,11 +17,11 @@ def twtTests():
     lines = file.readlines()
 
     i = 0
-    while (i < 5):
+    while (i < 914): #number of lines in the file, can be automated but I'm lazy 
         actualTweet = lines[i] + lines[i+1]
         api.update_status(status = actualTweet,)
         i += 2
-        sleep(5)
+        sleep(28800)
 
 if __name__ == '__main__':
     twtTests()
